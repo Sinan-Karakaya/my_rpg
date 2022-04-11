d48099dae4aaa1ef54a3011e6aa4b98166c4a756
RoheeAxel <axel.rohee@epitech.eu>
2022-04-11 12:52:06 +0200

    Feat (core.c): remove malloc of world from core and add it to init_world

--- src/map/calcul/calcul.c
+++ src/map/calcul/calcul.c
@@ -16,7 +16,7 @@ sfVector2f to2d(sfVector3f p, rpg_t *game)
     sfVector2f point_2d = {0,0};
 
     y_divide = ((y - (z -  game->cam.y) * game->cam.r) / (z - game->cam.y));
-    point_2d.x = 300*((x - game->cam.x)/(z- game->cam.y))+960;
+    point_2d.x = 300 * ((x - game->cam.x)/(z- game->cam.y))+960;
     point_2d.y = 300 * y_divide + 540;
     return point_2d;
 }
