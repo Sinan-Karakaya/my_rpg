57a61cbd00134d18f4a11b54d925c648e0be24f9
SinanFeisar <sinan.karakaya@epitech.eu>
2022-04-18 14:02:30 +0200

    fix(menu/init.c): added malloc checks

--- src/menu/init/init.c
+++ src/menu/init/init.c
@@ -10,27 +10,27 @@
 
 int init_menu(rpg_t *rpg)
 {
-    rpg->menu = malloc(sizeof(menu_t));
-    rpg->menu->main = malloc(sizeof(menu_main_t));
-    rpg->menu->option = malloc(sizeof(menu_option_t));
+    if (!(rpg->menu = malloc(sizeof(menu_t))) ||
+    !(rpg->menu->main = malloc(sizeof(menu_main_t))) ||
+    !(rpg->menu->option = malloc(sizeof(menu_option_t))))
+        return 84;
     rpg->menu->main->far_g_sprite = sfSprite_create();
     rpg->menu->main->far_g_texture = sfTexture_createFromFile("assets/menu/background.png", NULL);
     if (!rpg->menu->main->far_g_texture || !rpg->menu->main->far_g_sprite)
         return 84;
     sfSprite_setTexture(rpg->menu->main->far_g_sprite, rpg->menu->main->far_g_texture, sfTrue);
     sfSprite_setScale(rpg->menu->main->far_g_sprite, (sfVector2f){6, 6});
-
     rpg->menu->main->back_g_sprite = sfSprite_create();
     rpg->menu->main->back_g_texture = sfTexture_createFromFile("assets/menu/farground.png", NULL);
     if (!rpg->menu->main->back_g_texture || !rpg->menu->main->back_g_sprite)
         return 84;
     sfSprite_setTexture(rpg->menu->main->back_g_sprite, rpg->menu->main->back_g_texture, sfTrue);
     sfSprite_setScale(rpg->menu->main->back_g_sprite, (sfVector2f){1.5, 1.5});
-
     rpg->menu->main->mid_g_sprite = sfSprite_create();
     rpg->menu->main->mid_g_texture = sfTexture_createFromFile("assets/menu/midground.png", NULL);
     if (!rpg->menu->main->mid_g_texture || !rpg->menu->main->mid_g_sprite)
         return 84;
     sfSprite_setTexture(rpg->menu->main->mid_g_sprite, rpg->menu->main->mid_g_texture, sfTrue);
     sfSprite_setScale(rpg->menu->main->mid_g_sprite, (sfVector2f){1.5, 1.5});
+    return 0;
 }
