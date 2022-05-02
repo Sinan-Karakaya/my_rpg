a9b2b5addabedffb7886ef28039e92d4d8a6e8ea
SinanFeisar <sinan.karakaya@epitech.eu>
2022-05-02 15:54:22 +0200

    norm(collision.c): put formulas in macros

--- include/defines.h
+++ include/defines.h
@@ -70,6 +70,16 @@
 #define SET_TEX sfSprite_setTexture
 #define TEX_CB rpg->world->gui.chatbox_texture,
 
+#define CALCUL_1(x, y, i, j) (sfVector2f){((point[1].x - point[0].x) / 150 * \
+                             (offset_tab[rpg->world->object_map[i][j]].y - \
+                             offset_tab[rpg->world->object_map[i][j]].x)), \
+                             -(point[1].x - point[0].x) * 0.1})
+
+#define CALCUL_2(x, y, i, j) (sfVector2f){point[0].x + ((point[1].x - \
+                             point[0].x) / 150 * \
+                             offset_tab[rpg->world->object_map[i][j]].x),\
+                             point[0].y})
+
 extern const int olberic_rect_left[26];
 extern const int olberic_rect_w[26];
 
