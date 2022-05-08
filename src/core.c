0eedec55e1956a8d51512b3f15d9b5c304cd1725
thomas <thomas.juin@epitech.eu>
2022-05-08 14:39:31 -0400

    Norm (core.c) fix an error

--- src/core.c
+++ src/core.c
@@ -25,7 +25,7 @@ int main(int ac, char **av, char **env)
             return 84;
     rpg->world->npc_list[0] = (npc_t){"", (sfVector2i){18, 18},
     1, 50, 20, 20, 20};
-    rpg->world->npc_list[1] =(npc_t){"", (sfVector2i){110, 75},
+    rpg->world->npc_list[1] = (npc_t){"", (sfVector2i){110, 75},
     2, 78, 78, 20, 20};
     fill_map(rpg);
     srand(time(NULL));
