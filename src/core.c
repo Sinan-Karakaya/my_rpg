03fbddf3398258c14a3386072e318a3a2263754a
thomas <thomas.juin@epitech.eu>
2022-05-08 17:20:48 -0400

    feat (core.c) Now npc is at the norm

--- src/core.c
+++ src/core.c
@@ -11,6 +11,20 @@
 
 #include "my_rpg.h"
 
+static npc_t cnpc(sfVector2i pos, sfVector2i value, sfVector2i value2, size_t i)
+{
+    npc_t my_npc;
+
+    my_npc.name = "";
+    my_npc.pos = pos;
+    my_npc.type = value.x;
+    my_npc.max_life = value.y;
+    my_npc.attack = value2.x;
+    my_npc.defense = value2.y;
+    my_npc.speed = i;
+    return my_npc;
+}
+
 int main(int ac, char **av, char **env)
 {
     int debug_mode = handle_args(ac, av);
@@ -23,10 +37,11 @@ int main(int ac, char **av, char **env)
     if (sfRenderWindow_isOpen(rpg->window))
         if (init_all(rpg) == 84)
             return 84;
-    rpg->world->npc_list[0] = (npc_t){"", (sfVector2i){18, 18},
-    1, 50, 20, 20, 20};
-    rpg->world->npc_list[1] = (npc_t){"", (sfVector2i){110, 75},
-    2, 78, 78, 20, 20};
+
+    rpg->world->npc_list[0] = cnpc((sfVector2i){18, 18}, (sfVector2i){1, 50},
+    (sfVector2i){20, 20}, 20);
+    rpg->world->npc_list[1] = cnpc((sfVector2i){110, 75}, (sfVector2i){2, 78},
+    (sfVector2i){78, 20}, 20);
     fill_map(rpg);
     srand(time(NULL));
     do_loop(rpg);
